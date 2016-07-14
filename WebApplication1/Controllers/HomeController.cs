29c995e9faceda97a7d186138ef9d9ab8e08c25e
wagdi <wagdi@sears.co.il>
2016-07-14 16:46:03 +0300

    adding some ifs

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web.Mvc;
 using ChatterBotAPI;
 using Newtonsoft.Json;
+using System.IO;//File, Directory, Path
 
 namespace WebApplication1.Controllers
 {
@@ -14,10 +15,14 @@ namespace WebApplication1.Controllers
 			var webClient = GetWebClient();
 			var answer = webClient.UploadValues(new Uri($"https://api.wit.ai/message?v=20160714&q={s}"), "POST", "");
 			var serializedAnswer = JsonConvert.DeserializeObject<AiNlipResponse>(answer);
-			if (serializedAnswer.entities.intent.FirstOrDefault().value == "greeting" && serializedAnswer._text.Contains(" you doing"))
-				return "Iam a robot, you pervert";
+			var intent = GetIntent(serializedAnswer);
+			var nlpResonse = SetResponseAccordingToIntent(intent, serializedAnswer);
+
+			if (nlpResonse != null)
+				return nlpResonse;
+
 			ChatterBotFactory factory = new ChatterBotFactory();
-			
+
 			ChatterBot bot2 = factory.Create(ChatterBotType.PANDORABOTS, "b0dafd24ee35a477");
 			ChatterBotSession bot2session = bot2.CreateSession();
 
@@ -28,6 +33,72 @@ namespace WebApplication1.Controllers
 			var s2 = bot2session.Think(s);
 
 			return s2;
+		}
+
+		private string SetResponseAccordingToIntent(string intent, AiNlipResponse response)
+		{
+			switch (intent)
+			{
+				case "greeting":
+					return ReturnGrettingMessage(response);
+				case "seeking gift":
+					return SeekingGift(response);
+				case "age_detection":
+					return AgeDetection();
+				case "money_amount":
+					return MoneyAcount();
+			}
+			return null;
+		}
+
+		private string MoneyAcount()
+		{
+			string statment = System.IO.File.ReadAllText("statment.txt");
+
+			if (statment == "third")
+			{
+				System.IO.File.WriteAllText("statment.txt", "first");
+				return "ok i will search a gift for your nience for the ammount of 50 dollars";
+			}
+			return null;
+		}
+
+		private string AgeDetection()
+		{
+			string statment = System.IO.File.ReadAllText("statment.txt");
+
+			if (statment == "second")
+			{
+				System.IO.File.WriteAllText("statment.txt", "third");
+				return "how much would you like to spend?";
+			}
+
+			return null;
+		}
+
+		private static string ReturnGrettingMessage(AiNlipResponse response)
+		{
+			if (response._text.Contains(" you doing")) return "Iam a robot, you pervert";
+			return null;
+		}
+
+		private string SeekingGift(AiNlipResponse response)
+		{
+			string statment = System.IO.File.ReadAllText("statment.txt");
+
+			if (statment == null || statment == "first")
+			{
+				System.IO.File.WriteAllText("statment.txt","second");
+				return $"how old is your {response.entities.recipient.FirstOrDefault().value}?";
+			}
+			return null;
+		}
+
+		private string GetIntent(AiNlipResponse serializedAnswer)
+		{
+			if (serializedAnswer.entities.intent.FirstOrDefault() == null)
+				return null;
+			return serializedAnswer.entities.intent.FirstOrDefault().value;
 		}
 
 		public class HomeModel
@@ -47,6 +118,7 @@ namespace WebApplication1.Controllers
 		public class Entities
 		{
 			public IntentResponse[] intent { get; set; }
+			public IntentResponse[] recipient { get; set; }
 
 		}
 
